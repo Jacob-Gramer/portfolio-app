5a71799b841f24c56bf7eba51939ff58448783a2
Jacob Gramer <grameraggie@gmail.com>
2023-02-16 12:15:49 -0600

    Added portfolios into their own PortfolioList and PortfolioItem components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { BlogList } from 'components/blogs';
+import { PortfolioList } from 'components/portfolios/';
 import Link from "next/link";
 import Image from 'next/legacy/image';
 
 import { Footer, Header, Navbar } from 'components/common';
 
 
-const portfolios = [
-  {
-    slug: "software-engineer-siemens",
-    title: "Software Engineer Siemens",
-    description: "Working as C++ and C# software engineer",
-    employmentDate: "2022-01-12",
-    coverImage: "https://images.unsplash.com/photo-1622473590773-f588134b6ce7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1370&q=50",
-  },
-]
+
 
 const Home: NextPage = () => {
   return (
@@ -29,13 +22,10 @@ const Home: NextPage = () => {
 
         <div className="relative">
           <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
-            {/* Navbar Starts */}
-            <Navbar />
-            {/* Navbar Ends */}
 
-            {/* Header Starts */}
+            <Navbar />
+
             <Header />
-            {/* Header Ends */}
 
           </div>
           <div className="relative lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
@@ -60,9 +50,7 @@ const Home: NextPage = () => {
               </Link>
           </h2>
 
-          {/* Blog List Starts */}
           <BlogList />
-          {/* Blog List Ends */}
 
           <br></br>
 
@@ -76,31 +64,8 @@ const Home: NextPage = () => {
               </Link>
           </h2>
 
-          {/* Portfolio List Starts */}
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {portfolios.map((portfolio) => (
-              <div key={portfolio.slug} className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                  <Image
-                    layout="fill"
-                    src={portfolio.coverImage}
-                    alt={""}
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <Link legacyBehavior href={`/portfolios/${portfolio.slug}`}>
-                    <a>
-                      <span className="absolute inset-0" />
-                      { portfolio.title }
-                    </a>
-                  </Link>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">{ portfolio.description }</p>
-              </div>
-            ))}
-          </div>
-          {/* Portfolio List Ends */}
+          <PortfolioList />
+
         </div>
       </div>
       <Footer />
