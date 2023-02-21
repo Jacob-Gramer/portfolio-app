5a088c55af8cefcfb39dec84ec283f39bcc557cf
Jacob Gramer <grameraggie@gmail.com>
2023-02-20 20:42:59 -0600

    Added resume to public folder and set up CV link in the header component to open up pdf resume in a new tab

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -19,7 +19,8 @@ const Header = () => {
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
-              href="#"
+              href="/cv.pdf"
+              target="_blank"
               className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg"
             >
               CV
